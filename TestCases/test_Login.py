2339c74844e51e595c3ab14d203b0b660b5a5cbb
maksudpranto <maksudpranto@gmail.com>
2025-05-01 08:40:30 +0600

    Added Login Test Cases and Validate Page Title after Login

--- TestCases/test_Login.py
+++ TestCases/test_Login.py
@@ -8,3 +8,12 @@ def test_loginPage_title_verify(setup_teardown):
     homeTitleVerify.title_verify()
 
 
+def test_performLogin(setup_teardown):
+    page = setup_teardown
+    login = LoginPage(page)
+    login.Login()
+
+def test_LoginValidation(setup_teardown):
+    page = setup_teardown
+    titleVerify_afterLogin = LoginPage(page)
+    titleVerify_afterLogin.validateLogin()
