85a27892425cc64774254c3599be9c1f93cd807c
Md Maksud Hossain Pranto <43799953+maksudpranto@users.noreply.github.com>
2025-06-15 12:29:19 +0600

    Update test_pizza_selection.py
    move page.goto from conftest to test case file

--- TestCases/test_pizza_selection.py
+++ TestCases/test_pizza_selection.py
@@ -3,6 +3,7 @@ from Pages.LoginPage import *
 
 def test_pizza_selection(setup_teardown):
     page = setup_teardown
+    page.goto('https://play1.automationcamp.ir/login.html')
     login = LoginPage(page)
     login.Login()
 
