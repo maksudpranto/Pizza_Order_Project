cc141f6bad42732cef0a32e4d6fa3921539521ab
Md Maksud Hossain Pranto <43799953+maksudpranto@users.noreply.github.com>
2025-06-15 12:51:31 +0600

    Update test_Login.py

--- TestCases/test_Login.py
+++ TestCases/test_Login.py
@@ -1,21 +1,18 @@
 from Pages.LoginPage import LoginPage
 
-def test_loginPage_title_verify(setup_teardown):
-    page = setup_teardown
-    page.goto('https://play1.automationcamp.ir/login.html')
+def test_loginPage_title_verify(login_test_fixture):
+    page = login_test_fixture
     homeTitleVerify = LoginPage(page)
     homeTitleVerify.title_verify()
 
 
-def test_performLogin(setup_teardown):
-    page = setup_teardown
-    page.goto('https://play1.automationcamp.ir/login.html')
+def test_performLogin(login_test_fixture):
+    page = login_test_fixture
     login = LoginPage(page)
     login.Login()
 
-def test_LoginValidation(setup_teardown):
-    page = setup_teardown
-    page.goto('https://play1.automationcamp.ir/login.html')
+def test_LoginValidation(login_test_fixture):
+    page = login_test_fixture
     titleVerify_afterLogin = LoginPage(page)
     titleVerify_afterLogin.validateLogin()
 
