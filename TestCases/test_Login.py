2138fed45362bb05a5d3dd500dcc62b783bb4d24
Md Maksud Hossain Pranto <43799953+maksudpranto@users.noreply.github.com>
2025-06-15 12:28:41 +0600

    Update test_Login.py
    Moved page.goto from conftest to test_login page.

--- TestCases/test_Login.py
+++ TestCases/test_Login.py
@@ -2,18 +2,20 @@ from Pages.LoginPage import LoginPage
 
 def test_loginPage_title_verify(setup_teardown):
     page = setup_teardown
-
+    page.goto('https://play1.automationcamp.ir/login.html')
     homeTitleVerify = LoginPage(page)
     homeTitleVerify.title_verify()
 
 
 def test_performLogin(setup_teardown):
     page = setup_teardown
+    page.goto('https://play1.automationcamp.ir/login.html')
     login = LoginPage(page)
     login.Login()
 
 def test_LoginValidation(setup_teardown):
     page = setup_teardown
+    page.goto('https://play1.automationcamp.ir/login.html')
     titleVerify_afterLogin = LoginPage(page)
     titleVerify_afterLogin.validateLogin()
 
