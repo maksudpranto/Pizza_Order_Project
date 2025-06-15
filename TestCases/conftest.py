2806f3c6331b0862c93122386aafdcc61f43e6f2
Md Maksud Hossain Pranto <43799953+maksudpranto@users.noreply.github.com>
2025-06-15 12:51:06 +0600

    Update conftest.py
    Added custom fixture for login test fixture

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from playwright.sync_api import sync_playwright
+from Pages.LoginPage import LoginPage
 
 
 @pytest.fixture(scope='function')
@@ -8,12 +9,13 @@ def setup_teardown():
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
-        # page.goto('https://play1.automationcamp.ir/login.html')
+        #page.goto('https://play1.automationcamp.ir/login.html')
         yield page
         context.close()
         browser.close()
 
-
-
-
-
+@pytest.fixture
+def login_test_fixture(setup_teardown):
+    page = setup_teardown
+    page.goto('https://play1.automationcamp.ir/login.html')
+    return page
